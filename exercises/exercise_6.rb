3f0ce020bdca1c236db8bdb4c4e8c94d0733b12f
Rafin Reza <rafintokyo@gmail.com>
2021-03-03 02:31:34 +0000

    associations

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -9,3 +9,9 @@ puts "Exercise 6"
 puts "----------"
 
 # Your code goes here ...
+
+@store1.employees.create(first_name: "Raf", last_name: "Reza", hourly_rate: 60)
+@store1.employees.create(first_name: "Farabi", last_name: "Ahmed", hourly_rate: 90)
+@store2.employees.create(first_name: "Will", last_name: "Murray", hourly_rate: 60)
+@store2.employees.create(first_name: "Corinne", last_name: "Beaulieu", hourly_rate: 100)
+
